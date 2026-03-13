df6520d775e868181ef72b1e1ab240937a53102b
Alexis Arias <aacedillo12@gmail.com>
2026-03-13 16:47:54 -0700

    Update create-user.ps1

--- scripts/create-user.ps1
+++ scripts/create-user.ps1
@@ -1,1 +1,12 @@
+Connect-AzureAD
 
+$passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$passwordProfile.Password = "TempP@ss1234"
+$passwordProfile.ForceChangePasswordNextLogin = $true
+
+New-AzureADUser `
+-DisplayName "Lab Engineer" `
+-UserPrincipalName labengineer@companylab.onmicrosoft.com `
+-AccountEnabled $true `
+-MailNickname "labengineer" `
+-PasswordProfile $passwordProfile
